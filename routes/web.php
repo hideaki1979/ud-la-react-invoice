d2a990315bcff71ba4b5ec1b3b900f626dd68d6b
HIDEAKI_KAGAMI <syumeikyo@outlook.jp>
2026-02-01 13:46:37 +0900

    sail環境構築、商品一覧実装

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -24,4 +25,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::middleware('auth')->group(function () {
+    Route::resource('products', ProductController::class);
+});
+
+require __DIR__ . '/auth.php';
